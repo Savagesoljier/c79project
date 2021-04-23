0c34c99038c072d5d3556812640bb8e5852d990a
Savagesoljier <78633767+Savagesoljier@users.noreply.github.com>
2021-04-23 12:42:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+menu_list_array = [
+    "chicken Tandoori Pizza",
+    "Veg Supreme Pizza",
+    "Paneer Tikka Pizza",
+    "Deluxe Veggie Pizza",
+    "Veg Extrazaganza Pizza"];
+
+    function getmenu(){
+        var htmldata;
+        htmldata="<ol class='menulist'>"
+        menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ '<li>' + menu_list_array[i] + '</li>'
+        }
+        htmldata=htmldata + "</ol>"
+        document.getElementById("display_menu").innerHTML = htmldata;
+    }
